9d6cc8981e4473e9b58c444094d08fb70ba79056
Naveen Kumar <Naveen.Kumar10@veritas.com>
2024-09-23 22:16:48 +0530

    removing hard coded value and putting in application.properties

--- src/main/java/org/health/assignment/healthzassignment/service/HealthCheck.java
+++ src/main/java/org/health/assignment/healthzassignment/service/HealthCheck.java
@@ -6,20 +6,27 @@ import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HealthCheck implements IHealthCheck {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheck.class);
-    private static final String MYSQL_URL = "jdbc:mysql://localhost:3306/mysql";
-    private static final String MYSQL_USER = "root";
-    private static final String MYSQL_PASSWORD = "";
+
+    @Value("${mysql.url}")
+    private String mysqlUrl;
+
+    @Value("${mysql.user}")
+    private String mysqlUserName;
+
+    @Value("${mysql.password}")
+    private String mysqlPassword;
 
     @Override
     public boolean checkDBConnectionStatus() {
         boolean isConnectionSuccessful = false;
-        try (Connection connection = DriverManager.getConnection(MYSQL_URL, MYSQL_USER, MYSQL_PASSWORD)) {
+        try (Connection connection = DriverManager.getConnection(mysqlUrl, mysqlUserName, mysqlPassword)) {
             if (connection.isValid(2)) {
                 LOGGER.info("Connection is successful.");
                 isConnectionSuccessful = true;
